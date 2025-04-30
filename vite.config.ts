62019d52023d6f15c69044a1adf89932fc9a4408
dodopontocom <rodolfotiago@gmail.com>
2025-04-29 22:50:46 -0300

    exclude util from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react', 'util'], // evita que util seja pré-processado
+  },
+  resolve: {
+    alias: {
+      util: false, // bloqueia importações de 'util'
+    },
+  },
+  define: {
+    'process.env': {}, // evita erros com process.env no browser
   },
 });
