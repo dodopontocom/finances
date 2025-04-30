58c8790668d69132cf064cc481327e159b41b250
dodopontocom <rodolfotiago@gmail.com>
2025-04-29 23:11:00 -0300

    util

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react', 'util'], // evita que util seja pré-processado
   },
-  resolve: {
-    alias: {
-      util: false, // bloqueia importações de 'util'
-    },
-  },
-  define: {
-    'process.env': {}, // evita erros com process.env no browser
-  },
 });
